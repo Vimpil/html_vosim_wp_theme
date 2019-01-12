b9bac2a9572082d15b028578f5f4ba94e56f80bd
Zack <vimpil@ya.ru>
2019-01-12 20:07:15 +0300

    front page wp migration

--- index.php
+++ index.php
@@ -17,6 +17,48 @@ if (shortcode_exists('button')) {
 }
 ?>
     <h2>Test MEnu</h2>
+
+    <!-- POSTS CODE -->
+
+    <?php if (have_posts()): ?>
+
+      <?php if (is_home() && !is_front_page()): ?>
+        <header>
+          <h1 class="page-title screen-reader-text"><?php single_post_title();?></h1>
+        </header>
+      <?php endif;?>
+
+      <?php
+// Start the loop.
+while (have_posts()):
+	the_post();
+
+	/*
+		         * Include the Post-Format-specific template for the content.
+		         * If you want to override this in a child theme, then include a file
+		         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+	*/
+	get_template_part('template-parts/content', get_post_format());
+
+	// End the loop.
+endwhile;
+
+// If no content, include the "No posts found" template.
+else:
+	get_template_part('template-parts/content', 'none');
+
+endif;
+?>
+
+<!-- END POSTS CODE -->
+<?php echo 'post' ?>
+<?php
+$get_content = get_post_by_name('main_slider')->post_content;
+ChromePhp::log($get_content);
+echo the_content(get_post_by_name('main_slider')->post_content);
+?>
+<?php echo 'post' ?>
+
     <?php
 echo '[button]';
 echo do_shortcode('[button]');
